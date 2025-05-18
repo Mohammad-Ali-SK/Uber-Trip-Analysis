b9bc6b47559f12b7ea7c001293e7e986a515c8e5
Mohammad Ali sk <mohammadalisk050@gmail.com>
2025-05-18 20:31:35 +0530

    add all file

---
+++ SQL Query.sql
@@ -0,0 +1,33 @@
+-- ✅  Exploratory SQL Analysis on Uber Trip Dataset --
+
+
+-- 🟢 Basic SQL Questions (1–7)
+
+-- 1. Retrieve all columns from the table.
+-- 2. Show all trips where the passenger count is greater than 2.
+-- 3. Get the total number of trips.
+-- 4. List all unique vehicle types used.
+-- 5. Find trips where fare amount is greater than 20.
+-- 6. Count the number of trips for each payment type.
+-- 7. Find the earliest and latest pickup times.
+
+-- ---
+
+-- 🟡 Intermediate SQL Questions (8–14)
+
+-- 8. Show average trip duration grouped by vehicle type.
+-- 9. Find total fare amount collected for each PU\_City.
+-- 10. List the top 5 most frequent pickup locations.
+-- 11. Get the average surge fee for each payment type.
+-- 12. Count how many trips were completed by each vehicle type.
+-- 13. Find trips that started in Manhattan and ended in Queens.
+-- 14. Calculate average trip distance per hour of the day.
+
+-- ---
+
+-- 🔵 Advanced SQL Questions (15–20)
+
+-- 15. Identify the top 3 pickup-dropoff location pairs with the highest number of trips.
+-- 16. Show total revenue (fare + surge) per city.
+-- 17. Rank payment types by total number of trips.
+-- 18. Calculate average fare per minute for each vehicle type.
